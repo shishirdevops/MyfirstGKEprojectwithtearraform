936accb4332e98afbca1deffecc591dc0f2747cb
shishirdevops <40268040+shishirdevops@users.noreply.github.com>
2022-01-03 01:51:10 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "cluster_name" {
+  description = "The name for the GKE cluster"
+  default     = "shishir-cluster"
+}
+variable "env_name" {
+  description = "The env"
+  default     = "k8s"
+}
+variable "region" {
+  description = "The region to host the cluster in"
+  default     = "europe-west1"
+}
+variable "network" {
+  description = "The VPC network created to host the cluster in"
+  default     = "gke-network"
+}
+variable "subnetwork" {
+  description = "The subnetwork created to host the cluster in"
+  default     = "gke-subnet"
+}
+variable "ip_range_pods_name" {
+  description = "The secondary ip range to use for pods"
+  default     = "ip-range-pods"
+}
+variable "ip_range_services_name" {
+  description = "The secondary ip range to use for services"
+  default     = "ip-range-services"
+}
